65590d816d3125bd00e8650a2d41494b15e4ec99
micklerj <micklerj@ufl.edu>
2024-09-02 15:38:05 -0400

    sesh

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
 // Trust the first proxy if behind a proxy or load balancer
-app.set('trust proxy', process.env.TRUST_PROXY || 1);
+app.set('trust proxy', 10);
 
 // session for managing access and refresh tokens
 app.use(session({          
@@ -37,8 +37,8 @@ app.use(session({
     secure: false,                // false when in development, true in production (for HTTPS)
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24, // 1 day
-    // sameSite: 'None', // Ensure cookies are sent across different domains
-    // path: '/' // Ensure the cookie is available on all routes
+    sameSite: 'None', // Ensure cookies are sent across different domains
+    path: '/' // Ensure the cookie is available on all routes
    }                             
 }))
 
